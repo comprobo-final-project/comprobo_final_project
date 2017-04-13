0a9df5fde0bd16638e23ec666e2d09b1c9fb5f54
kzhang8850 <kevin.zhang@students.olin.edu>
2017-04-13 16:37:54 -0400

    fixed april to be a provider

--- comprobo_final_project/scripts/april_tags.py
+++ comprobo_final_project/scripts/april_tags.py
@@ -1,40 +1,42 @@
 #!usr/bin/env python
 
 """
-Got Apriltags absolute position
+Retrieves Apriltags absolute position, and acts as an importable package
 """
 
-import rospy
-from geometry_msgs.msg import Twist, Point, PoseStamped
+from geometry_msgs.msg import PoseStamped
 import tf
 
-class April(object):
+class AprilPoseProvider(object):
     """
-    Basically for now it just takes data from the Apriltags and can send it somewhere else
+    Takes data from the Apriltags and can sends it to RobotController
     """
 
     def __init__(self):
-        rospy.init_node("april")
 
         rospy.Subscriber("/STAR_pose_continuous", PoseStamped, self.getPos, queue_size=10)
-
-        self.position = None
+        self.pose_callback = None
         self.orient = None
 
-    def getPos(self, data):
+
+    def getPos(self, april_data):
         """
-        just takes the position and does something with it
+        Takes the pose and ships it off to the currently assigned callback (which should be in RobotController)
         """
 
-        self.position = data.pose.position.x, data.pose.position.y, data.pose.position.z
-        self.orient = data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w
+        self.pose_callback(april_data.pose)
+
+        #if we ever want it, orientation is here too
+        self.orient = april_data.pose.orientation.x, april_data.pose.orientation.y, april_data.pose.orientation.z, april_data.pose.orientation.w
         eulers = tf.transformations.euler_from_quaternion(self.orient)
 
 
-        print "pos", self.position
-        print "orient", self.orient
-        print "eulers", eulers
-        print " "
+    def subscribe(self, pose_callback):
+        """
+        updates new callback function for packaging purposes
+        """
+        
+        self.pose_callback = pose_callback
 
 
     def run(self):
