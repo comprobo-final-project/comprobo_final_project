afcea6b7890cace2c2518badbe33c8a21f28c4a8
hdavidzhu <hdavidzhu@gmail.com>
2017-04-17 10:37:56 -0400

    Upgraded to Vector3 and fixed angular twist.

--- comprobo_final_project/scripts/simulator/pose.py
+++ comprobo_final_project/scripts/simulator/pose.py
@@ -1,6 +1,8 @@
-from vector_2 import Vector2
+from vector_3 import Vector3
+from quaternion import Quaternion
 
 class Pose:
 
     def __init__(self):
-        self.position = Vector2()
+        self.position = Vector3()
+        self.orientation = Quaternion()
---
+++ comprobo_final_project/scripts/simulator/vector_3.py
@@ -0,0 +1,20 @@
+class Vector3:
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.z = 0
+
+    def __rmul__(self, other):
+        result = Vector3()
+        result.x = other * self.x
+        result.y = other * self.y
+        result.z = other * self.z
+        return result
+
+    def __radd__(self, other):
+        result = Vector3()
+        result.x = other.x + self.x
+        result.y = other.y + self.y
+        result.z = other.z + self.z
+        return result
