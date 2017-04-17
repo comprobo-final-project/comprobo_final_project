322fc0ed71ba38e07b9c1839e28d407b5806c6e9
hdavidzhu <hdavidzhu@gmail.com>
2017-04-17 14:15:40 -0400

    Introduced angular velocity.

--- comprobo_final_project/scripts/simulator/robot.py
+++ comprobo_final_project/scripts/simulator/robot.py
@@ -1,3 +1,5 @@
+import math
+
 from pose import Pose
 from twist import Twist
 
@@ -13,4 +15,13 @@ class Robot:
 
 
     def step(self, step_size):
-        self.pose.position += step_size * self.twist.linear
+        twist_r = self.twist.linear.x
+        twist_theta = self.twist.angular.z
+        original_velocity_theta = math.atan2(self.pose.velocity.y, self.pose.velocity.x)
+
+        # Update velocity
+        self.pose.velocity.x = twist_r * math.cos(original_velocity_theta + step_size * twist_theta)
+        self.pose.velocity.y = twist_r * math.sin(original_velocity_theta + step_size * twist_theta)
+
+        # Update position
+        self.pose.position += step_size * self.pose.velocity
